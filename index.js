ba4abdc71785b1b29bed9c3bc0ef378a1205c41c
Anurag Gupta <anurag6967@gmail.com>
2023-08-26 12:24:44 +0530

    Anurag third commit

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express=require('express');
 const port=8004;
 const app=express();
-const router=require('./routes')
+const router=require('./routes/index')
 
 
-//Use express router
+//Use express router middleware.................
 app.use('/',router);
 
-app.get('/home',(req,res)=>{
-    return res.send(`<h1>Home Page Of Major Project ${0+1}</h1>`);9
+
+
+
+
+// simple route (/home)..................
+app.get('/simple',(req,res)=>{
+    return res.send(`<h1>Home Page Of Major Project Simple ${0+1}</h1>`);9
 })
 
+
+//server listining on port 8004
 app.listen(port,(err)=>{
     if(err){
       console.log(`Error while connection to server ${err}`); //using interpolation(backtick)
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express=require('express');
 
 const router=express.Router();
 
+const homeController=require("../controllers/home_controller")
+
 console.log("router Losded");
+
+//home router
+router.get('/home',homeController.home);
 
 
 
