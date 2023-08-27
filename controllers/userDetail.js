2e972ee7fc50a8fe4a57ae565d58279d6e7e6716
Anurag Gupta <anurag6967@gmail.com>
2023-08-27 22:36:34 +0530

    some changes inside home.ejs

--- controllers/userDetail.js
+++ controllers/userDetail.js
@@ -1,5 +1,5 @@
 
 module.exports.detail=function(req,res){
-    return res.render('first');
+    return res.render('home',{title:"HOME EJS FILE RENDERING"});
     // return res.end("<h1>Users Details</h1>");
 }
